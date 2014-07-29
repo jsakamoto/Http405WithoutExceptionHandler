dff972f3db964584f47092a3d106748d1892fa99
jsakamoto <junichi.jp@gmail.com>
2014-07-29 12:05:48 +0900

    1st commit

---
+++ Http405WithoutExceptionHandler/Http405WithoutExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Toolbelt.Web
+{
+    public class Http405WithoutExceptionHandler : IHttpHandler
+    {
+        public bool IsReusable { get { return true; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.StatusCode = 405;
+        }
+    }
+}
